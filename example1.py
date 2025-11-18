78b3733c671ebbe004eefd18202b1c19383f0cea
Luca Mosetti <luka.mosetti@gmail.com>
2025-11-18 17:06:47 +0100

    more or less...

---
+++ example1.py
@@ -0,0 +1,48 @@
+from pysmt.shortcuts import (
+    GE,
+    LE,
+    LT,
+    And,
+    Bool,
+    Iff,
+    Ite,
+    Plus,
+    Pow,
+    Real,
+    Symbol,
+    Times,
+)
+from pysmt.typing import BOOL, REAL
+
+from wmpy.solvers import WMISolver
+
+# variables definition
+a = Symbol("A", BOOL)
+x1 = Symbol("x1", REAL)
+x2 = Symbol("x2", REAL)
+
+# formula definition
+phi = Bool(True)
+
+# weight function definition
+# fmt: off
+w = Plus(Ite(GE(x1, Real(0)),
+             Pow(x1, Real(3)),
+             Times(Real(-2), x1)),
+         Ite(a,
+             Times(Real(3), x2),
+             Times(Real(-1), Pow(x2, Real(5)))))
+
+chi = And(LE(Real(-1), x1), LT(x1, Real(1)),
+          LE(Real(-1), x2), LT(x2, Real(1)),
+          Iff(a, GE(x2, Real(0))))
+# fmt: on
+
+print("Formula:", phi.serialize())
+print("Weight function:", w.serialize())
+print("Support:", chi.serialize())
+print()
+
+wmi = WMISolver(chi, w)
+result, n_integrations = wmi.compute(phi, {x1, x2})
+print("WMI \t result = {}, \t # integrations = {}".format(result, n_integrations))
--- flake.nix
+++ flake.nix
@@ -1,55 +1,18 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    d4.url = "github:SoftVarE-Group/d4v2/2.3.2";
   };
 
   outputs = {
     self,
     nixpkgs,
-    d4,
   }: let
-    system = "x86_64-linux";
+    system = "x86_64-darwin";
     pkgs = nixpkgs.legacyPackages.${system};
   in {
-    packages.${system} = {
-      patoh = pkgs.pkgsStatic.callPackage ./nix/patoh.nix {};
-
-      c2d = pkgs.pkgsStatic.callPackage ./nix/c2d.nix {};
-
-      mathsat = pkgs.pkgsStatic.callPackage ./nix/mathsat.nix {};
-
-      tabularAllSMT = pkgs.pkgsStatic.callPackage ./nix/tabularAllSMT.nix {};
-
-      image = pkgs.dockerTools.buildLayeredImage {
-        name = "playground";
-        tag = "latest";
-        contents = [
-          pkgs.python310
-          pkgs.gcc
-          pkgs.bashInteractive
-          self.packages.${system}.c2d
-          self.packages.${system}.mathsat
-          self.packages.${system}.tabularAllSMT
-          self.packages.${system}.patoh
-          d4.packages.${system}.d4
-        ];
-        config = {
-          Entrypoint = ["/bin/sh"];
-        };
-      };
-    };
-
     devShells.${system}.default = pkgs.mkShell {
       buildInputs = [
-        pkgs.python313
-        pkgs.gcc
-        pkgs.gmp
-        self.packages.${system}.c2d
-        self.packages.${system}.mathsat
-        self.packages.${system}.tabularAllSMT
-        self.packages.${system}.patoh
-        d4.packages.${system}.d4
+        pkgs.python314
       ];
     };
   };
