0c7550b3ec5ead508b5f0162e4ec2d4f014d5099
Luca Mosetti <luka.mosetti@gmail.com>
2025-12-15 23:04:49 +0100

    improve logging

--- src/benchmarks/benchmarks-run.py
+++ src/benchmarks/benchmarks-run.py
@@ -7,6 +7,9 @@ from datetime import timedelta
 from pathlib import Path
 from subprocess import CompletedProcess, TimeoutExpired
 
+from math import prod
+from tqdm import tqdm
+
 handler: logging.Handler = logging.StreamHandler(sys.stderr)
 handler.setFormatter(logging.Formatter('%(asctime)s :: %(levelname)-4s :: %(message)s'))
 
@@ -23,40 +26,57 @@ if __name__ == '__main__':
     results: Path = rsc / 'results'
     results.mkdir(exist_ok=True)
 
-    enumerators = ['sae', 'd4', 'cudd']
-    integrators = ['noop']
-    densities = (rsc / 'densities').rglob('*.json', recurse_symlinks=True)
+    enumerators: list[str] = ['sae', 'd4', 'cudd']
+    integrators: list[str] = ['noop']
+    densities: list[Path] = list((rsc / 'densities').rglob('*.json', recurse_symlinks=True))
 
-    for enumerator, integrator, density in it.product(enumerators, integrators, densities):
+    with tqdm(
+            iterable=it.product(enumerators, integrators, densities),
+            total=prod(map(len, (enumerators, integrators, densities))),
+            ascii=' ⠁⠃⠇⠏⠟⠯⠷⠿⡿⣟⣯⣷⣾⣿',
+            bar_format='{bar} {n_fmt}/{total_fmt} {elapsed}⌶{remaining}',
+    ) as todo:
 
-        if 0 == os.path.getsize(density):
-            logger.warning(f'skipping {density}')
-            continue
+        for enumerator, integrator, density in todo:
 
-        logger.info(f"{enumerator} {integrator} {density.name}")
+            if 0 == os.path.getsize(density):
+                logger.warning(f'skipping {density}')
+                continue
 
-        with (
-            open(results / f'{enumerator}-{integrator}-{density.name}-stdout.ndjson', 'wt') as out,
-            open(results / f'{enumerator}-{integrator}-{density.name}-stderr.ndjson', 'wt') as err,
-        ):
-            try:
-                wmi: CompletedProcess[str] = (
-                    subprocess.run(
-                        [
-                            'python',
-                            '-m',
-                            'src.wmi',
-                            f'--density={density.as_posix()}',
-                            f'--enumerator={enumerator}',
-                            f'--integrator={integrator}'
-                        ],
-                        cwd=wdr,
-                        text=True,
-                        timeout=timedelta(minutes=1).total_seconds(),
-                        stdout=out,
-                        stderr=err,
+            # todo.set_postfix({'e': enumerator, 'i': integrator, 'd': density.name})
+
+            path_err = results / f'{enumerator}-{integrator}-{density.name}-stderr.ndjson'
+            path_out = results / f'{enumerator}-{integrator}-{density.name}-stdout.ndjson'
+
+            with (
+                open(path_out, 'wt') as out,
+                open(path_err, 'wt') as err,
+            ):
+                try:
+                    wmi: CompletedProcess[str] = (
+                        subprocess.run(
+                            [
+                                'python',
+                                '-m',
+                                'src.wmi',
+                                f'--density={density.as_posix()}',
+                                f'--enumerator={enumerator}',
+                                f'--integrator={integrator}'
+                            ],
+                            cwd=wdr,
+                            text=True,
+                            timeout=timedelta(minutes=1).total_seconds(),
+                            stdout=out,
+                            stderr=err,
+                        )
                     )
-                )
 
-            except TimeoutExpired as e:
-                logger.warning(f'{enumerator} {integrator} {density} timed out')
+                except TimeoutExpired as e:
+                    logger.warning(f'{enumerator} {integrator} {density} timed out')
+
+            if 0 != wmi.returncode:
+                path_err.rename(path_err.parent / f'{enumerator}-{integrator}-{density.name}-stderr.log')
+                path_out.rename(path_out.parent / f'{enumerator}-{integrator}-{density.name}-stdout.log')
+                logger.error(f'{enumerator} {integrator} {density} non-zero exit')
+                path_err.touch()
+                path_out.touch()
