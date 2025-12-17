94455b48bc1d50ef54c373e04dc1d41799771aa5
Luca Mosetti <luka.mosetti@gmail.com>
2025-12-17 14:21:16 +0100

    precompute tlemmas

--- src/benchmarks/benchmarks-run.py
+++ src/benchmarks/benchmarks-run.py
@@ -18,6 +18,20 @@ logger.setLevel(logging.DEBUG)
 logger.propagate = False
 logger.addHandler(handler)
 
+
+def extract_ndjson(path: Path):
+    with (
+        open(path.rename(path.with_suffix('.log')), 'rt') as log,
+        open(path, 'wt') as ndjson,
+    ):
+        ndjson.writelines(
+            it.takewhile(
+                lambda line: line.startswith('{'),
+                log,
+            )
+        )
+
+
 if __name__ == '__main__':
     wdr: Path = Path(__file__).parent.parent.parent
     src: Path = wdr / 'src'
@@ -26,9 +40,59 @@ if __name__ == '__main__':
     results: Path = rsc / 'results'
     results.mkdir(exist_ok=True)
 
+    tlemmas: Path = results / 'tlemmas'
+    tlemmas.mkdir(exist_ok=True)
+
     enumerators: list[str] = ['sae', 'd4', 'sdd']
     integrators: list[str] = ['noop']
-    densities: list[Path] = list((rsc / 'densities').rglob('*.json', recurse_symlinks=True))
+    densities: list[Path] = [
+        density
+        for density in (rsc / 'densities').rglob('*.json', recurse_symlinks=True)
+        if 0 != os.path.getsize(density)
+    ]
+
+    with tqdm(
+            iterable=densities,
+            ascii=' ⠁⠃⠇⠏⠟⠯⠷⠿⡿⣟⣯⣷⣾⣿',
+            bar_format='{bar} {n_fmt}/{total_fmt} {elapsed}/{remaining}',
+    ) as todo:
+
+        for density in todo:
+
+            path_err = tlemmas / f'{density.name}-stderr.ndjson'
+            path_out = tlemmas / f'{density.name}.smt2'
+
+            if all(path.exists() for path in (path_out, path_err)):
+                logger.warning(f'skipping computing tlemmas for {density}')
+                continue
+
+            with (
+                open(path_err, 'wt') as err,
+            ):
+                try:
+                    p4tlemmas: CompletedProcess[str] = (
+                        subprocess.run(
+                            [
+                                'python',
+                                '-m',
+                                'src.tlemmas',
+                                f'--density={density.as_posix()}',
+                                f'--tlemmas={path_out}',
+                            ],
+                            cwd=wdr,
+                            text=True,
+                            timeout=timedelta(minutes=20).total_seconds(),
+                            stdout=None,
+                            stderr=err,
+                        )
+                    )
+
+                    if 0 != p4tlemmas.returncode:
+                        logger.error(f'tlemmas {density} non-zero exit')
+
+                except TimeoutExpired:
+                    logger.warning(f'tlemmas {density} timed out')
+                    extract_ndjson(path_err)
 
     with tqdm(
             iterable=it.product(enumerators, integrators, densities),
@@ -39,21 +103,18 @@ if __name__ == '__main__':
 
         for enumerator, integrator, density in todo:
 
-            if 0 == os.path.getsize(density):
-                logger.warning(f'skipping {density}')
-                continue
-
             # todo.set_postfix({'e': enumerator, 'i': integrator, 'd': density.name})
 
             path_err = results / f'{enumerator}-{integrator}-{density.name}-stderr.ndjson'
             path_out = results / f'{enumerator}-{integrator}-{density.name}-stdout.ndjson'
+            path_tlemmas = tlemmas / f'{density.name}.smt2'
 
             with (
                 open(path_out, 'wt') as out,
                 open(path_err, 'wt') as err,
             ):
                 try:
-                    wmi: CompletedProcess[str] = (
+                    p4wmi: CompletedProcess[str] = (
                         subprocess.run(
                             [
                                 'python',
@@ -61,20 +122,22 @@ if __name__ == '__main__':
                                 'src.wmi',
                                 f'--density={density.as_posix()}',
                                 f'--enumerator={enumerator}',
-                                f'--integrator={integrator}'
+                                f'--integrator={integrator}',
+                                f'--tlemmas={path_tlemmas.as_posix()}',
                             ],
                             cwd=wdr,
                             text=True,
-                            timeout=timedelta(minutes=1).total_seconds(),
+                            timeout=timedelta(minutes=10).total_seconds(),
                             stdout=out,
                             stderr=err,
                         )
                     )
 
-                    if 0 != wmi.returncode:
-                        path_err.rename(path_err.parent / f'{enumerator}-{integrator}-{density.name}-stderr.log')
-                        path_err.touch()
+                    if 0 != p4wmi.returncode:
                         logger.error(f'{enumerator} {integrator} {density} non-zero exit')
+                        path_err.rename(path_err.with_suffix('.log'))
+                        path_err.touch()
 
-                except TimeoutExpired as e:
+                except TimeoutExpired:
                     logger.warning(f'{enumerator} {integrator} {density} timed out')
+                    extract_ndjson(path_err)
---
+++ src/tlemmas.py
@@ -0,0 +1,89 @@
+import argparse
+import logging
+import os
+import sys
+import typing as t
+from dataclasses import dataclass
+from pathlib import Path
+from time import perf_counter
+
+import pysmt.shortcuts as smt
+from pysmt.fnode import FNode
+from pysmt.shortcuts import write_smtlib
+from theorydd.solvers.lemma_extractor import extract
+from theorydd.solvers.mathsat_partial_extended import MathSATExtendedPartialEnumerator
+from wmpy.cli.density import Density
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+logger.propagate = False
+logger.addHandler(logging.StreamHandler(sys.stderr))
+
+
+@dataclass
+class Log:
+    step: str
+
+    def __enter__(self):
+        self.start: float = perf_counter()
+
+    def __exit__(self, *_):
+        dump_step(self.step, perf_counter() - self.start)
+
+
+def dump_step(step: str, took: float) -> None:
+    logger.debug('{"step": "%s", "took": %f}', step, took)
+
+
+def read_density(file: Path) -> Density:
+    with Log(f'parsing density'):
+        density = Density.from_file(file.as_posix())
+        density.add_bounds()
+        return density
+
+
+def times(whatever: dict[str, dict | float]) -> t.Iterable[tuple[str, float]]:
+    """
+    >>> list(extract({ 'alpha time': 1.0, 'alpha': { 'beta time': 2.0, 'beta': { 'gamma time': 3.0 } } }))
+    [('alpha time', 1.0), ('beta time', 2.0), ('gamma time', 3.0)]
+    """
+
+    for key, value in whatever.items():
+
+        match value:
+            case float() if 'time' in key:
+                yield key, value
+
+            case dict():
+                yield from times(value)
+
+
+if __name__ == '__main__':
+    parser: argparse.ArgumentParser = argparse.ArgumentParser()
+    parser.add_argument('--density', type=Path, required=True)
+    parser.add_argument('--tlemmas', type=Path, required=True)
+    args: argparse.Namespace = parser.parse_args()
+
+    density: Density = read_density(args.density)
+    formula: FNode = smt.And(density.support, smt.Bool(True))
+
+    with Log('extract tlemmas'):
+        computations: dict = {}
+
+        tlemmas: list[FNode]
+        _, tlemmas, _ = extract(
+            formula,
+            MathSATExtendedPartialEnumerator(
+                computation_logger=computations,
+                parallel_procs=os.process_cpu_count(),
+            )
+        )
+
+        for step, took in times(computations):
+            dump_step(step.removesuffix(' time'), took)
+
+    with Log('writing tlemmas'):
+        write_smtlib(
+            smt.And(tlemmas),
+            args.tlemmas
+        )
