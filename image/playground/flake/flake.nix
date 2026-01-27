ad64818e479873f8bf6c0ff0a792c56fd6de1999
Luca Mosetti <luka.mosetti@gmail.com>
2026-01-27 10:51:08 +0100

    remove unused stuff

--- image/playground/flake/flake.nix
+++ image/playground/flake/flake.nix
@@ -12,15 +12,11 @@
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
     pkgs-self = self.packages.${system};
-    i686 = pkgs.pkgsi686Linux;
     craneLib = crane.mkLib pkgs;
   in {
     packages.${system} = {
-      c2d = i686.callPackage ./nix/c2d {};
       d4 = pkgs.callPackage ./nix/d4 {};
       mathsat = pkgs.callPackage ./nix/mathsat {};
-      tabularAllSMT = pkgs.callPackage ./nix/tabularAllSMT {};
-      volesti-integrate = pkgs.callPackage ./nix/volesti {};
       decdnnf_rs = pkgs.callPackage ./nix/decdnnf_rs {inherit craneLib;};
 
       bundle = pkgs.buildEnv {
@@ -30,11 +26,8 @@
           pkgs.graphviz
           pkgs.latte-integrale
           pkgs.z3
-          pkgs-self.c2d
           pkgs-self.d4
           pkgs-self.mathsat
-          pkgs-self.tabularAllSMT
-          pkgs-self.volesti-integrate
           pkgs-self.decdnnf_rs
         ];
       };
